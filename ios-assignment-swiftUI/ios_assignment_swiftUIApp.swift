370fbd251bdd228be6521e24c6b43310ca0ff93c
Hugo <hugo.boscducros@gmail.com>
2022-06-26 09:34:57 +0200

    Initial Commit

---
+++ ios-assignment-swiftUI/ios_assignment_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_assignment_swiftUIApp.swift
+//  ios-assignment-swiftUI
+//
+//  Created by Hugo Bosc-Ducros on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_assignment_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
